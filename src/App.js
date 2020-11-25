79643e7d714dd3305f8f1f627f97f8c575892253
elbancud <elbancud@gmail.com>
2020-11-25 21:34:16 +0800

    initial commit useEffect using todo List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState,useRef } from 'react';
+
 
 function App() {
+  const fileUpload = " ";
+  const [number, setNumber] = useState(0);
+  const [container, setContainer] = useState([]);
+  const [input, setInput] = useState("");
+  const handleClick = event => {
+    event.preventDefault();
+     const value = {
+      id: Math.random() + 1,
+      inputObject: input
+    }
+    setContainer([...container, value]);
+
+   
+    setInput(''); 
+  };
+
+  const uploadedFile = event =>
+  {
+    fileUpload = event.target.files;
+  }
+
+  useEffect(() => { 
+  console.log("hello");
+  
+},[number]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="file" name="filename" ref= {fileUpload }></input>
+        <input type="submit " onChange={uploadedFile}></input>
+      </form>
+
+      {console.log(fileUpload.files)}
     </div>
+
   );
 }
 
